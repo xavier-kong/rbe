f40ffb2c1bf14ae99603c8f76602c2f60860dd48
xavier-kong <kongxianchengxavier@gmail.com>
2023-06-26 20:43:56 +0800

    variable bindings

--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,23 @@
-// An attribute to hide warnings for unused code.
-#![allow(dead_code)] #[derive(Debug)]
-struct Person {
-    name: String,
-    age: u8,
-}
-
-// A unit struct
-struct Unit;
-
-// A tuple struct
-struct Pair(i32, f32);
-
-// A struct with two fields
-struct Point {
-    x: f32,
-    y: f32,
-}
-
-// Structs can be reused as fields of another struct
-struct Rectangle {
-    // A rectangle can be specified by where the top left and bottom right
-    // corners are in space.
-    top_left: Point, bottom_right: Point,
-}
-
-fn rect_area(rect: Rectangle) -> f32 {
-    let Rectangle { top_left: Point { x: left_edge, y: top_edge }, 
-        bottom_right: Point { x: right_edge, y: bottom_edge }
-    } = rect;
-    let length = right_edge - left_edge;
-    let height = top_edge - bottom_edge;
-    return length * height
-}
-
-
-fn square(pt: Point, length: f32) -> Rectangle {
-    let top_left: Point = pt;
-    let mut bottom_right: Point = Point { x: top_left.x, y: top_left.y };
-
-    bottom_right.x += length;
-    bottom_right.y -= length;
-
-    Rectangle {
-        top_left,
-        bottom_right,
-    }
-}
-
 fn main() {
-    // Create struct with field init shorthand
-    let name = String::from("Peter");
-    let age = 27;
-    let peter = Person { name, age };
+    // Declare a variable binding
+    let a_binding;
 
-    // Print debug struct
-    println!("{:?}", peter);
+    {
+        let x = 2;
 
-    // Instantiate a `Point`
-    let point: Point = Point { x: 10.3, y: 0.4 };
+        // Initialize the binding
+        a_binding = x * x;
+    }
 
-    // Access the fields of the point
-    println!("point coordinates: ({}, {})", point.x, point.y);
+    println!("a binding: {}", a_binding);
 
-    // Make a new point by using struct update syntax to use the fields of our
-    // other one
-    let bottom_right = Point { x: 5.2, ..point };
+    let another_binding;
 
-    // `bottom_right.y` will be the same as `point.y` because we used that field
-    // from `point`
-    println!("second point: ({}, {})", bottom_right.x, bottom_right.y);
+    // Error! Use of uninitialized binding
+    //println!("another binding: {}", another_binding);
+    // FIXME ^ Comment out this line
 
-    // Destructure the point using a `let` binding
-    let Point { x: left_edge, y: top_edge } = point;
+    another_binding = 1;
 
-    let _rectangle = Rectangle {
-        // struct instantiation is an expression too
-        top_left: Point { x: left_edge, y: top_edge },
-        bottom_right,
-    };
-
-    // Instantiate a unit struct
-    let _unit = Unit;
-
-    // Instantiate a tuple struct
-    let pair = Pair(1, 0.1);
-
-    // Access the fields of a tuple struct
-    println!("pair contains {:?} and {:?}", pair.0, pair.1);
-
-    // Destructure a tuple struct
-    let Pair(integer, decimal) = pair;
-
-    println!("pair contains {:?} and {:?}", integer, decimal);
-
-
-
+    println!("another binding: {}", another_binding);
 }
